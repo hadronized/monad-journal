3997fff29df88e9a6853be33144002210e760994
Daniel Gröber <dxld@darkboxed.org>
2014-07-22 00:23:59 +0200

    Add mtl class instances for JournalT

--- Control/Monad/Trans/Journal.hs
+++ Control/Monad/Trans/Journal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, UndecidableInstances #-}
 
 {- |
 Module      :  Control.Monad.Trans.Journal
@@ -19,18 +19,40 @@ module Control.Monad.Trans.Journal (
   , module X
   ) where
 
-import Control.Applicative ( Applicative )
+import Control.Applicative ( Applicative, Alternative )
+import Control.Monad ( MonadPlus )
 import Control.Monad.Journal.Class as X
-import Control.Monad.Trans ( MonadTrans, MonadIO )
-import Control.Monad.Trans.State ( StateT, get, modify, put, runStateT )
+import Control.Monad.Trans ( MonadTrans, MonadIO, lift )
+import Control.Monad.Trans.State ( StateT, get, modify, put, runStateT, mapStateT )
+
+import Control.Monad.Reader.Class ( MonadReader(..) )
+import Control.Monad.Writer.Class ( MonadWriter(..) )
+import Control.Monad.State.Class  ( MonadState )
+import qualified Control.Monad.State.Class as MS ( MonadState(..) )
+
 import Data.Monoid ( Monoid(..) )
 
-newtype JournalT w m a = JournalT (StateT w m a) deriving (Applicative,Functor,Monad,MonadTrans,MonadIO)
+newtype JournalT w m a = JournalT (StateT w m a)
+    deriving ( Applicative
+             , Alternative
+             , Functor
+             , Monad
+             , MonadTrans
+             , MonadIO
+             , MonadPlus
+             , MonadReader r
+             , MonadWriter w'
+             )
 
 instance (Monoid w, Monad m) => MonadJournal w (JournalT w m) where
   journal !w = JournalT . modify $ flip mappend w
   history = JournalT get
   clear   = JournalT (put mempty)
+
+instance MonadState s m => MonadState s (JournalT w m) where
+    get = lift MS.get
+    put = lift . MS.put
+    state = lift . MS.state
 
 runJournalT :: (Monoid w, Monad m) => JournalT w m a -> m (a,w)
 runJournalT (JournalT s) = runStateT s mempty
